581e3963b26e98ce054bcf7f0db5c1b85849708c
Bence Tamas <mr.bence.tamas@gmail.com>
2014-03-26 16:24:31 +0100

    added columns app with initial feincms configs

---
+++ columns/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from feincms.module.page.models import Page
+from feincms.content.richtext.models import RichTextContent
+from feincms.content.medialibrary.models import MediaFileContent
+
+Page.register_extensions(
+    'feincms.module.page.extensions.navigation',
+    'feincms.module.page.extensions.titles',
+    'feincms.module.extensions.translations',
+    'feincms.module.extensions.datepublisher',
+    ) # Example set of extensions
+
+Page.register_templates({
+    'title': _('Standard template'),
+    'path': 'base.html',
+    'regions': (
+        ('main', _('Main content area')),
+        #('sidebar', _('Sidebar'), 'inherited'),
+        ),
+    })
+
+Page.create_content_type(RichTextContent)
+Page.create_content_type(MediaFileContent, TYPE_CHOICES=(
+    ('default', _('default')),
+    ('lightbox', _('lightbox')),
+))
+
+
