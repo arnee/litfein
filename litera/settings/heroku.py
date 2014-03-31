4c270c16a599071cd5df42b705c4090eea903c80
benctamas <mr.bence.tamas@gmail.com>
2014-03-31 17:26:25 +0200

    updated s3 settings (allow public read)

--- litera/settings/heroku.py
+++ litera/settings/heroku.py
@@ -20,6 +20,8 @@ SOUTH_DATABASE_ADAPTERS = {
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "")
+AWS_S3_SECURE_URLS = False
+AWS_QUERYSTRING_AUTH = False
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
